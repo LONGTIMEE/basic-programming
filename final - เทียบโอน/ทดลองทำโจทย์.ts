d90a9af550e041491175efdb5655d06dec3eaf3d
LONGTIMEE <armachitphol@gmail.com>
2024-10-05 14:28:09 +0700

    s

--- final - เทียบโอน/ทดลองทำโจทย์.ts
+++ final - เทียบโอน/ทดลองทำโจทย์.ts
@@ -27,3 +27,8 @@ function covertToArrayStringWithMap(arr: number[]) {
         return element.toString();
     })
 }    
+
+
+console.log(covertToArrayString([1,2,3,4,5]))
+console.log(covertToArrayString2([1,2,3,4,5]))
+console.log(covertToArrayStringWithMap([1,2,3,4,5]))
