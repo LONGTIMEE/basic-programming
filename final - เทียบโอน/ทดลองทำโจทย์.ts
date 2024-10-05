b35b5453c439d363e33b8c6fa3c56fa6de539fb4
LONGTIMEE <armachitphol@gmail.com>
2024-10-05 14:19:59 +0700

    s

--- final - เทียบโอน/ทดลองทำโจทย์.ts
+++ final - เทียบโอน/ทดลองทำโจทย์.ts
@@ -1,3 +1,18 @@
 function covertToArrayString(arr: number[]) {
     
+    // ยาก
+    const newArray: string[] = [];
+    for(let i = 0; i < arr.length; i++) {
+        newArray.push(arr[i].toString())
+    }
+}
+
+
+function covertToArrayString2(arr: number[]) {
+    
+    // ยาก
+    const newArray: string[] = [];
+    for(let i = 0; i < arr.length; i++) {
+        newArray.push(arr[i].toString())
+    }
 }
